62c5c7b11b7004061bf2ee7e1b18229598db7d90
liming <limingcisco@163.com>
2020-05-01 22:10:04 +0800

    初始化提交

---
+++ src/com/company/test/hashmap/Test.java
@@ -0,0 +1,13 @@
+package com.company.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<String,Double> hashmap=new HashMap<>();
+        hashmap.put("k2",0.2);
+        hashmap.put("k3",0.3);
+        hashmap.put("k4",0.4);
+    }
+}
